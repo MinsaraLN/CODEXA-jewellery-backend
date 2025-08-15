95046e3e11eab1c08bd1e15aea606712c28241d1
MinsaraLN <it24103215@my.sliit.lk>
2025-08-15 13:19:07 +0530

    Initial Commit

---
+++ src/main/java/com/CODEXA/jewellery/CodexaJewelleryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.CODEXA.jewellery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexaJewelleryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodexaJewelleryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CODEXA/jewellery/CodexaJewelleryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CODEXA.jewellery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodexaJewelleryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
